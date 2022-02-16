89674a0c3f26cd8f49eba72c9df075f1ad89e0b4
talhamohsin <talhamohsin50@gmail.com>
2022-02-16 13:12:43 +0100

    Base structure refactored

--- app/views.py
+++ app/views.py
@@ -1,25 +1,26 @@
 import pandas as pd
-from flask import Flask, render_template, request
+from flask import Blueprint, render_template, request
+from . import db
 
-app = Flask(__name__, template_folder='templates')
+views = Blueprint('views', __name__, template_folder='templates')
 
-topics = pd.read_csv('../data/output/test_adhoc2005narrative.tsv', sep='\t', header=0, names=['id', 'desc'], dtype='str')
+topics = pd.read_csv('data/output/test_adhoc2005narrative.tsv', sep='\t', header=0, names=['id', 'desc'], dtype='str')
 topics = topics.dropna()
-ref_documents = pd.read_csv('../data/output/topic_reference_and_documents.tsv', sep='\t', header=0)
-TREC_corpus = pd.read_csv('../data/output/TREC_test_corpus.tsv', sep='\t', header=0)
+ref_documents = pd.read_csv('data/output/topic_reference_and_documents.tsv', sep='\t', header=0)
+TREC_corpus = pd.read_csv('data/output/TREC_test_corpus.tsv', sep='\t', header=0)
 TREC_corpus = TREC_corpus.set_index('PMID')
 TREC_corpus['title'] = TREC_corpus['title'].astype('str')
 TREC_corpus['abstract'] = TREC_corpus['abstract'].astype('str')
 
 
-@app.route('/')
-@app.route('/topics')
+@views.route('/')
+@views.route('/topics')
 def topic_overview():
         topics_overview = topics.set_index('id')
         topicsList = topics['id'].tolist()
         return render_template('topics_overview.html', topics=topics_overview, topicsList=topicsList)
 
-@app.route('/<int:topic_id>')
+@views.route('/<int:topic_id>')
 def topic(topic_id):
         topic_description = topics['desc'][topics['id']==str(topic_id)].reset_index(drop=True)
         topic_desc = topic_description[0]
@@ -32,7 +33,7 @@ def topic(topic_id):
         return render_template('topic.html', topic=topic_desc, topic_id = topic_id, article_list=article_list, TREC_corpus = TREC_corpus)
 
 
-@app.route('/<int:topic_id>/<int:ref_pmid>', methods=['GET', 'POST'])
+@views.route('/<int:topic_id>/<int:ref_pmid>', methods=['GET', 'POST'])
 def ref_article(ref_pmid, topic_id):
         topic_description = topics['desc'][topics['id']==str(topic_id)].reset_index(drop=True)
         topic_desc = topic_description[0]
@@ -49,7 +50,7 @@ def ref_article(ref_pmid, topic_id):
         
         return render_template('ref_article.html', topic=topic_desc, topic_id=topic_id, ref_pmid=ref_pmid, title=ref_title, abstract=ref_abstract, article_list=docList_chunked, TREC_corpus = TREC_corpus)
 
-@app.route('/<int:topic_id>/<int:ref_pmid>/<int:pmid>', methods=['GET', 'POST'])
+@views.route('/<int:topic_id>/<int:ref_pmid>/<int:pmid>', methods=['GET', 'POST'])
 def assessment_article(pmid, ref_pmid, topic_id):
         topic_description = topics['desc'][topics['id']==str(topic_id)].reset_index(drop=True)
         topic_desc = topic_description[0]
@@ -66,14 +67,9 @@ def assessment_article(pmid, ref_pmid, topic_id):
         percent_complete =round(((index_active+1)/n_articles)*100)
         article_title = TREC_corpus.at[pmid, 'title']
         article_abstract = TREC_corpus.at[pmid, 'abstract']
+
         return render_template("article.html", topic=topic_desc, topic_id=topic_id, title=article_title, abstract=article_abstract, pmid=pmid, ref_pmid=ref_pmid, article_list=docList, TREC_corpus = TREC_corpus, n_articles=n_articles, index_active=index_active, index_previous=index_previous, index_next=index_next, percent_complete=percent_complete)
 
-@app.route("/login")
-def login():
-        return render_template("login.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
 
 
 
