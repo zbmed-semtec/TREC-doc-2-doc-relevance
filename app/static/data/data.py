dc6b5ad29c81a13bf6f3bcba5d2776079f6f5253
talhamohsin <talhamohsin50@gmail.com>
2022-03-17 22:45:05 +0100

    Relative path fixed

--- app/static/data/data.py
+++ app/static/data/data.py
@@ -1,16 +1,16 @@
 import pandas as pd
 
 topics = pd.read_csv(
-    "static/data/test_adhoc2005narrative.tsv",
+    "app/static/data/test_adhoc2005narrative.tsv",
     sep="\t",
     header=0,
     names=["id", "desc"],
     dtype="str",
 )
 ref_documents = pd.read_csv(
-    "static/data/topic_reference_and_documents.tsv", sep="\t", header=0
+    "app/static/data/topic_reference_and_documents.tsv", sep="\t", header=0
 )
-TREC_corpus = pd.read_csv("static/data/TREC_test_corpus.tsv", sep="\t", header=0)
+TREC_corpus = pd.read_csv("app/static/data/TREC_test_corpus.tsv", sep="\t", header=0)
 ref_documents = ref_documents[
     ref_documents["PMID to be assessed"].isin(TREC_corpus["PMID"])
 ]  # Drop all articles where we dont have a title or article
